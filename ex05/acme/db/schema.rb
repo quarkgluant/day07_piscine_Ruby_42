75d2cdca4232e0cdd9e2b94dd5a68592367a6434
Patrice Cadiot <pathibul.r@gmail.com>
2019-12-02 10:42:37 +0100

    ex05  add the files generated by `rails g cancan:ability` and `rails generate rolify Role User`, and add partial gitignore

--- ex05/acme/db/schema.rb
+++ ex05/acme/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191127140542) do
+ActiveRecord::Schema.define(version: 20191202093630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(version: 20191127140542) do
 
   add_index "products", ["brand_id"], name: "index_products_on_brand_id", using: :btree
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "name",                   default: "", null: false
@@ -66,6 +76,13 @@ ActiveRecord::Schema.define(version: 20191127140542) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
   add_foreign_key "cart_items", "carts"
   add_foreign_key "cart_items", "products"
